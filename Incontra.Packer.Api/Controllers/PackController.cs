3ed4a0bcb830e0ae06a1fa8358301d03b18b73d5
wiktor.wilczynski <w@wilczy.net>
2016-03-25 10:02:28 +0100

    Refactor

--- Incontra.Packer.Api/Controllers/PackController.cs
+++ Incontra.Packer.Api/Controllers/PackController.cs
@@ -124,13 +124,13 @@ namespace Incontra.Packer.Api.Controllers
         private List<InputContainer> GetJsonContainers(List<Container> containers, Models.PackRequest request)
         {
             var packContainers = new List<InputContainer>();
-            var calculation = new Data.Model.PackRequest
+            var packRequest = new Data.Model.PackRequest
             {
                 UserID = 1,
                 RequestDate = DateTime.Now,
                 CalculationTime = _executionTime
             };
-            calculation = _packerService.PackRequestInsert(calculation);
+            //packRequest = _packerService.PackRequestInsert(packRequest);
             _spaceTotal = 0;
             _spaceUsed = 0;
             foreach (var container in containers)
@@ -138,18 +138,7 @@ namespace Incontra.Packer.Api.Controllers
                 _weightTotal += container.Weight;
                 _spaceTotal += container.Width * container.Height * container.Depth;
                 var packContainer = ModelFactory.Create(container);
-                var containerGuid = Guid.NewGuid();
-                string contents = containerGuid.ToString();
-                var container3dContent = String.Format(@"
-                    var containerMesh = new BABYLON.Mesh.CreateBox(""containerMesh"", 1, scene);
-                    containerMesh.scaling = new BABYLON.Vector3({0}+0.2, {1}+0.2, {2}+0.2);
-                    containerMesh.material = containerMaterial;
-                    self.drawEdges(scene, containerMesh, new BABYLON.Color3(0, 0, 0), 0.1);"
-                    , container.Width
-                    , container.Height
-                    , container.Depth                    
-                    );
-
+                var containerGuid = Guid.NewGuid();                                
                 var spaceUsedInContainer = 0;
                 var spaceInContainer = container.Width * container.Height * container.Depth;
                 var weightOfBoxesInContainer = 0.0;
@@ -160,59 +149,89 @@ namespace Incontra.Packer.Api.Controllers
                     spaceUsedInContainer += box.Width * box.Height * box.Depth;
                     weightOfBoxesInContainer += box.Weight;
                     var packBox = ModelFactory.Create(box);
-                    packContainer.boxes.Add(packBox);
-                    container3dContent += String.Format(@"
-                        var boxMesh = new BABYLON.Mesh.CreateBox(""boxMesh"", 1, scene);
-                        boxMesh.scaling = new BABYLON.Vector3({0},{1},{2});
-                        boxMesh.position = new BABYLON.Vector3({3}-{6}/2+{0}/2, {4}-{7}/2+{1}/2, {5}-{8}/2+{2}/2);
-                        boxMesh.material = self.createBoxMaterial(scene, {9}, {10}, {11}, 1);
-                        self.drawEdges(scene, boxMesh, new BABYLON.Color3(0, 0, 0), 0.5);"
-                        , box.Width
-                        , box.Height
-                        , box.Depth
-                        , box.X
-                        , box.Y
-                        , box.Z
-                        , container.Width
-                        , container.Height
-                        , container.Depth 
-                        , box.R.ToString().Replace(',', '.')
-                        , box.G.ToString().Replace(',', '.')
-                        , box.B.ToString().Replace(',', '.')
-                        );
+                    packContainer.boxes.Add(packBox);                    
                 }
 
                 if (request.options.showSpaceUsage)
                     packContainer.spaceUsage = String.Format("{0}%", Math.Round((decimal)spaceUsedInContainer / spaceInContainer * 100, 2));
                 if (request.options.showTotalWeight)
                     packContainer.weight = String.Format("{0}{1}", Math.Round(weightOfBoxesInContainer,4), request.options.weightUnit);
-                
-                var path = System.Web.HttpContext.Current.Server.MapPath("/Content/Visualizers/");
-                
-                var folder = String.Format("{0}-{1}{2}-{3}{4}"
-                    , DateTime.Today.Year
-                    , DateTime.Today.Month < 10 ? "0" : String.Empty
-                    , DateTime.Today.Month
-                    , DateTime.Today.Day < 10 ? "0" : String.Empty
-                    , DateTime.Today.Day
-                    );
-                var container3dFile = String.Format("container.3d.{0}.html", containerGuid);
-                var container3dTemplate = File.ReadAllText(Path.Combine(path, "container.3d.html"));
-                var cameraDistance = Math.Max(container.Width, Math.Max(container.Height, container.Depth)) * 2;
-                container3dContent = String.Format(container3dTemplate, container3dContent, cameraDistance);
-                if (!Directory.Exists(Path.Combine(path,folder)))
-                    Directory.CreateDirectory(Path.Combine(path, folder));
-                File.WriteAllText(Path.Combine(Path.Combine(path, folder), container3dFile), container3dContent);
 
-                var visualizer3D = string.Format(@"<iframe id=""3dp"" width=""300"" height=""300"" style=""border: none;"" src=""http://localhost:53675/{0}""></iframe>"
-                    , String.Format("Content/Visualizers/{0}/container.3d.{1}.html"
-                        , folder
-                        , containerGuid));
+                var containerVisualizerFolder = GetDateFolderName();
+                var containerVisualizerFile = String.Format("container.3d.{0}.html", containerGuid);
+                var containerVisualizerContent = GetContainerVisualizerContent(container);
+                SaveContainerVisualizerFile(container, containerVisualizerContent, containerVisualizerFolder, containerVisualizerFile);
+
+                var visualizer3D = string.Format(@"
+                    <iframe id=""3dp"" width=""300"" height=""300"" style=""border: none;"" src=""{0}://{1}:{2}/Content/Visualizers/{3}/{4}""></iframe>"
+                    , Request.RequestUri.Scheme
+                    , Request.RequestUri.Host
+                    , Request.RequestUri.Port
+                    , containerVisualizerFolder
+                    , containerVisualizerFile);
                    
                 packContainer.view3d = visualizer3D;
                 packContainers.Add(packContainer);
             }
             return packContainers;
+        }
+
+        private string GetContainerVisualizerContent(Container container)
+        {              
+            var containerVisualizerContent = String.Format(@"
+                var containerMesh = new BABYLON.Mesh.CreateBox(""containerMesh"", 1, scene);
+                containerMesh.scaling = new BABYLON.Vector3({0}+0.2, {1}+0.2, {2}+0.2);
+                containerMesh.material = containerMaterial;
+                self.drawEdges(scene, containerMesh, new BABYLON.Color3(0, 0, 0), 0.1);"
+                , container.Width
+                , container.Height
+                , container.Depth
+                );                
+            foreach (var box in container.Boxes)
+            {
+                containerVisualizerContent += String.Format(@"
+                    var boxMesh = new BABYLON.Mesh.CreateBox(""boxMesh"", 1, scene);
+                    boxMesh.scaling = new BABYLON.Vector3({0},{1},{2});
+                    boxMesh.position = new BABYLON.Vector3({3}-{6}/2+{0}/2, {4}-{7}/2+{1}/2, {5}-{8}/2+{2}/2);
+                    boxMesh.material = self.createBoxMaterial(scene, {9}, {10}, {11}, 1);
+                    self.drawEdges(scene, boxMesh, new BABYLON.Color3(0, 0, 0), 0.5);"
+                    , box.Width
+                    , box.Height
+                    , box.Depth
+                    , box.X
+                    , box.Y
+                    , box.Z
+                    , container.Width
+                    , container.Height
+                    , container.Depth
+                    , box.R.ToString().Replace(',', '.')
+                    , box.G.ToString().Replace(',', '.')
+                    , box.B.ToString().Replace(',', '.')
+                    );
+            }
+            return containerVisualizerContent;
+        }
+
+        private void SaveContainerVisualizerFile(Container container, string containerVisualizerContent, string containerVisualizerFolder, string containerVisualizerFile)
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath("/Content/Visualizers/");           
+            var container3dTemplate = File.ReadAllText(Path.Combine(path, "container.3d.html"));
+            var cameraDistance = Math.Max(container.Width, Math.Max(container.Height, container.Depth)) * 2;
+            containerVisualizerContent = String.Format(container3dTemplate, containerVisualizerContent, cameraDistance);
+            if (!Directory.Exists(Path.Combine(path, containerVisualizerFolder)))
+                Directory.CreateDirectory(Path.Combine(path, containerVisualizerFolder));
+            File.WriteAllText(Path.Combine(Path.Combine(path, containerVisualizerFolder), containerVisualizerFile), containerVisualizerContent);
+        }
+
+        private string GetDateFolderName()
+        {
+            return String.Format("{0}-{1}{2}-{3}{4}"
+                , DateTime.Today.Year
+                , DateTime.Today.Month < 10 ? "0" : String.Empty
+                , DateTime.Today.Month
+                , DateTime.Today.Day < 10 ? "0" : String.Empty
+                , DateTime.Today.Day
+                );
         }
     }
 }
